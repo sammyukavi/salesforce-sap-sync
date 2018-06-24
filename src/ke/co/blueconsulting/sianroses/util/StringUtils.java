147d1b7ce2302614c0aa9e4ef2c13bd779da263d
sammyukavi <sammyukavi@gmail.com>
2018-06-24 13:01:39 +0300

    Removed unused methods

--- src/ke/co/blueconsulting/sianroses/util/StringUtils.java
+++ src/ke/co/blueconsulting/sianroses/util/StringUtils.java
@@ -1,119 +1,67 @@
 package ke.co.blueconsulting.sianroses.util;
 
-import org.apache.commons.validator.routines.InetAddressValidator;
-import org.apache.commons.validator.routines.UrlValidator;
-
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.Document;
-import javax.swing.text.JTextComponent;
-import java.beans.PropertyChangeEvent;
-import java.util.Objects;
-
 public class StringUtils {
-  private static final String NULL_AS_STRING = "null";
   private static final String SPACE_CHAR = " ";
-  private final static String NON_THIN = "[^iIl1\\.,']";
   
-  public static boolean isValidURL(String url) {
-    String[] schemes = {"http", "https"};
-    UrlValidator urlValidator = new UrlValidator(schemes);
-    return urlValidator.isValid(url);
-  }
-  
-  public static boolean isValidIP(String ip) {
-    InetAddressValidator addressValidator = new InetAddressValidator();
-    return addressValidator.isValid(ip);
-  }
-  
-  public static boolean notNull(String string) {
-    return null != string && !NULL_AS_STRING.equals(string.trim());
-  }
-  
-  public static boolean isBlank(String string) {
-    return null == string || SPACE_CHAR.equals(string);
-  }
-  
-  public static boolean notEmpty(String string) {
-    return !isNullOrEmpty(string);
-  }
-  
-  public static boolean isNullOrEmpty(String string) {
-    return string == null || string.isEmpty();
-  }
-  
-  public static void addChangeListener(JTextComponent text, ChangeListener changeListener) {
-    Objects.requireNonNull(text);
-    Objects.requireNonNull(changeListener);
-    DocumentListener dl = new DocumentListener() {
-      private int lastChange = 0, lastNotifiedChange = 0;
-      
-      @Override
-      public void insertUpdate(DocumentEvent e) {
-        changedUpdate(e);
-      }
-      
-      @Override
-      public void removeUpdate(DocumentEvent e) {
-        changedUpdate(e);
-      }
-      
-      @Override
-      public void changedUpdate(DocumentEvent e) {
-        lastChange++;
-        SwingUtilities.invokeLater(() -> {
-          if (lastNotifiedChange != lastChange) {
-            lastNotifiedChange = lastChange;
-            changeListener.stateChanged(new ChangeEvent(text));
-          }
-        });
-      }
-    };
-    text.addPropertyChangeListener("document", (PropertyChangeEvent e) -> {
-      Document d1 = (Document) e.getOldValue();
-      Document d2 = (Document) e.getNewValue();
-      if (d1 != null) d1.removeDocumentListener(dl);
-      if (d2 != null) d2.addDocumentListener(dl);
-      dl.changedUpdate(null);
-    });
-    Document d = text.getDocument();
-    if (d != null) d.addDocumentListener(dl);
+  /**
+   * Checks if a string input is blank
+   *
+   * @param stringInput String to check if is blank
+   * @return <b>True</b> if string input is blank <b>False</b> if string input is not blank
+   */
+  public static boolean isBlank(String stringInput) {
+    return null == stringInput || SPACE_CHAR.equals(stringInput);
   }
   
   /**
-   * Force-inserts line breaks into an otherwise human-unfriendly long string.
+   * Checks if a string input is null or empty
+   *
+   * @param stringInput String to check if is blank
+   * @return <b>True</b> if string input is null or empty <b>False</b> if string input is not null or empty
    */
-  private static String breakLongString(String input, int charLimit) {
-    String output = "", rest = input;
+  public static boolean isNullOrEmpty(String stringInput) {
+    return stringInput == null || stringInput.isEmpty();
+  }
+  
+  /**
+   * Insert line breaks into long string.
+   *
+   * @param stringInput The string to break
+   * @param charLimit   The numbers of characters to be used to count to break the long string
+   * @return <b>String</b> The broken concatenated string
+   */
+  public static String breakLongString(String stringInput, int charLimit) {
+    StringBuilder output = new StringBuilder();
+    String rest = stringInput;
     int i = 0;
     
-    // validate.
     if (rest.length() < charLimit) {
-      output = rest;
+      output = new StringBuilder(rest);
     } else if (!rest.equals("") && (rest != null))  // safety precaution
     {
-      do {    // search the next index of interest.
+      do {
         i = rest.lastIndexOf(" ", charLimit) + 1;
         if (i == -1)
           i = charLimit;
         if (i > rest.length())
           i = rest.length();
-        
-        // break!
-        output += rest.substring(0, i) + "\n";
+        output.append(rest.substring(0, i)).append("\n");
         rest = rest.substring(i);
       }
       while ((rest.length() > charLimit));
-      output += rest;
+      output.append(rest);
     }
     
-    return output;
+    return output.toString();
   }
   
-  public static String breakLongString(String message) {
-    return breakLongString(message, 240);
+  /**
+   * Insert line breaks into long string.
+   *
+   * @param stringInput The string to break
+   * @return <b>String</b> The broken concatenated string
+   */
+  public static String breakLongString(String stringInput) {
+    return breakLongString(stringInput, 240);
   }
 }
